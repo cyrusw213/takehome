32b22c59428928cb3a534e5d141c40b65ff22576
Cyrus Wickersham <cyruswickersham@Cyruss-MacBook-Air.local>
2022-08-02 13:32:45 -0400

    updated requirements.txt

--- takehomeassessment/settings.py
+++ takehomeassessment/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 # for secret key
 import os
 
-from decouple import config
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 API_KEY = config('API_KEY')
 LIST_ID = config('LIST_ID')
@@ -29,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
